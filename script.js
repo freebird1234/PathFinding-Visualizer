3bdd369c229750d7738cd3ae4169790370403b19
freebird1234 <67512596+freebird1234@users.noreply.github.com>
2021-10-05 03:49:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,45 @@ var totalRows = 25;
 var totalCols = 40;
 var table = document.getElementById("table-container");
 var isMouseDown = false;
+var inProgress = false;
 startElement = [10, 5];
 endElement = [10, 35];
 let dir = [-1, 0, 1, 0, -1];
+
+
+
+
+/*
+
+Create Legend
+*/
+
+var colorList = {Unvisited: 'whitesmoke', Wall: 'black', Path: 'Yellow' , Visited: 'Purple' , Start: 'Green' , End: 'Red'};
+
+colorize = function(colorList) {
+    var container = document.getElementById('Legend');
+  
+    for (var key in colorList) {
+        var boxContainer = document.createElement("div");
+        boxContainer.className="legend-box";
+        var box = document.createElement("div");
+        var label = document.createElement("span");
+        //label.style ="height: 20px;width: 20px;margin: 10px 10px;"
+        label.innerHTML = key;
+        box.className = "box";
+        box.style.backgroundColor = colorList[key];
+
+        boxContainer.appendChild(box);
+        boxContainer.appendChild(label);
+
+        container.appendChild(boxContainer);
+
+   }
+}
+
+colorize(colorList);
+
+
 
 /*
 
@@ -36,6 +72,7 @@ var myGrid = generateGrid(totalRows, totalCols);
 table.insertAdjacentHTML("beforeend", myGrid);
 
 let elementsArray = document.querySelectorAll("td");
+
 //Mark Start element
 let index = getIndex(startElement);
 elementsArray[index].className = "start";
@@ -70,9 +107,8 @@ Let's do Pathfinding
 */
 let stack = [];
 
-function DFS(elementsArray, startElement , visited) {
+function DFS(elementsArray, startElement, visited) {
   let ind = getIndex(startElement);
-
 
   if (
     startElement[1] >= totalCols ||
@@ -80,7 +116,7 @@ function DFS(elementsArray, startElement , visited) {
     startElement[0] < 0 ||
     startElement[1] < 0 ||
     elementsArray[ind].className == "Wall" ||
-    visited[startElement[0]][startElement[1]]==1
+    visited[startElement[0]][startElement[1]] == 1
   ) {
     return false;
   }
@@ -89,12 +125,16 @@ function DFS(elementsArray, startElement , visited) {
     stack.push(ind);
     return true;
   }
-  visited[startElement[0]][startElement[1]]=1;
+  visited[startElement[0]][startElement[1]] = 1;
+  
+  if(elementsArray[ind].className!="start")
+  elementsArray[ind].className = "visited";
+ 
   for (let i = 0; i < 4; i++) {
     let nextCell = [0, 0];
     nextCell[0] = startElement[0] + dir[i];
     nextCell[1] = startElement[1] + dir[i + 1];
-    let option1 = DFS(elementsArray, nextCell,visited);
+    let option1 = DFS(elementsArray, nextCell, visited);
 
     if (option1) {
       stack.push(ind);
@@ -104,32 +144,31 @@ function DFS(elementsArray, startElement , visited) {
   return false;
 }
 
-function delay(){
-  
-setTimeout(function(){writeNumber.html("1")},1000);
-setTimeout(function(){writeNumber.html("2")},2000);
-setTimeout(function(){writeNumber.html("3")},3000);
+function DFSPathFinding() {
+  let visited = new Array(totalRows)
+    .fill(0)
+    .map(() => new Array(totalCols).fill(0));
+  inProgress = true;
+  DFS(elementsArray, startElement, visited);
+
+  var intervalId = setInterval(function () {
+    if (stack.length <= 0) {
+      clearInterval(intervalId);
+    }
+
+    let i = stack.pop();
+    if (
+      elementsArray[i].className != "start" && elementsArray[i].className != "end"
+    ) {
+      elementsArray[i].className = "path";
+    }
+  }, 10 );
 }
 
- function DFSPathFinding(){
-   // console.log(startElement)
-    let visited = new Array(totalRows).fill(0).map(() => new Array(totalCols).fill(0));
-    DFS(elementsArray , startElement,visited);
-        delay();
-    while(stack.length >0){
-        delay();
-          let i = stack.pop();
-          if(elementsArray[i].className !="start" && elementsArray[i].className!="end"){
-          elementsArray[i].className="path";
-          
-          }
-          
-    }
- }
-
-
- document.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-     DFSPathFinding();
-    }
-  })
+document.addEventListener("keyup", (event) => {
+  if (event.code === "Space") {
+    inProgress=true;
+    DFSPathFinding();
+    inProgress=false;
+  }
+});
